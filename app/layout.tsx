a5d127604c4e90fc1f4f19f50ceffc9ebac09158
Sahil Sahu <sahilsahu731709@gmail.com>
2025-11-02 16:56:59 +0530

    setup convex db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -27,7 +28,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={outfit.className}>
-          <Provider>{children}</Provider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
